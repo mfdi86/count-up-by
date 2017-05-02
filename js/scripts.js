2caa86c59f22fe3dcdb0d927e23b6d517cfac261
Dylan Dills and Maria Del Castillo <dylan@>
2017-05-02 11:25:13 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Backend Logic //
+  var total = 0;
+  var summands = [5, 10, 15, 20, 25, 30]
+  for (var index = 0; index < summands.length; index += 5) {
+  total += summands[index];
+}
+alert(total);
+});
+
+var total = 0;
+var summands = [5, 10, 15, 20, 25, 30]
+for (var index = 0; index < summands.length; index += 5) {
+total += summands[index];
+}
+alert(total);
+});
