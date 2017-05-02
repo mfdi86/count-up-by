6951c29ccee811bbd8a87b228079e57cf9e4b27f
Dylan Dills and Maria Del Castillo <dylan@>
2017-05-02 13:10:54 -0700

    activate JavaScript and utilize the for method to loop the array and use multiples to count up to a number

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-// Backend Logic //
-  var total = 0;
-  var summands = [5, 10, 15, 20, 25, 30]
-  for (var index = 0; index < summands.length; index += 5) {
-  total += summands[index];
-}
-alert(total);
-});
-
-var total = 0;
-var summands = [5, 10, 15, 20, 25, 30]
-for (var index = 0; index < summands.length; index += 5) {
-total += summands[index];
-}
-alert(total);
+$ ( document ).ready(function() {
+  $('#counting').submit(function(event){
+    event.preventDefault();
+    var stop1 = parseInt($("#stop1").val());
+    var mult1 = parseInt($("#mult1").val());
+    for (var index = 0; index <= stop1 ; index += mult1) {
+    }
+    alert();
+  })
 });
